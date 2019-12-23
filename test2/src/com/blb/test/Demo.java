31eff41e3c190fb270bcf256b95175f00a508f86
2274650112 <xueyy_java@163.com>
2019-12-23 20:22:01 +0800

    第一次提交

---
+++ test2/src/com/blb/test/Demo.java
@@ -0,0 +1,10 @@
+package com.blb.test;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hello word");
+		System.out.println("abc");
+		System.out.println("hehe");
+		System.out.println("aaa");
+	}
+}
